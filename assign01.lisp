b53e11f71cc7ebb8860830a492c764181ef0eb57
ssowemim <ssowemim@ualberta.ca>
2016-01-18 22:19:46 -0700

    Testing quiz question

--- assign01.lisp
+++ assign01.lisp
@@ -8,4 +8,12 @@
 			(t(xmember X (cdr Y)))
 	)
 		      
-			
+
+;Testing quiz question 8
+		(defun xcount (X)
+			(cond
+			 	((null X) 0)
+			 	((equal null (rest X)) 0)
+			 	(t(+ 1 xcount(rest(rest X))))
+			 )
+		)
