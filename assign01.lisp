9d07b7f0f70ca10937b79063e768be82cb9b8f89
ssowemim <ssowemim@ualberta.ca>
2016-01-18 22:51:06 -0700

    Question two done

--- assign01.lisp
+++ assign01.lisp
@@ -1,19 +1,28 @@
 ;Starting assignment One
 ;QUESTION ONE
-
+;
 	(defun xmember (X Y)
 		(cond
 			((null Y) nil)
 			((equal X (car Y)) T)
 			(t(xmember X (cdr Y)))
 	)
-		      
 ;
-;Testing quiz question 8
-		(defun xcount (X)
-			(cond
-			 	((null X) 0)
-			 	((equal null (rest X)) 0)
-			 	(t(+ 1 xcount(rest(rest X))))
-			 )
+;
+;QUESTION TWO
+;
+	(defun flatten (X)
+		(cond 
+			((null X) nil)
+			((atom (car x))
+					(append
+					(list (car x))
+					(flatten (cdr x)))
+			)
+			(t(append
+				flatten (car x))
+				flatten (cdr x))
+
 		)
+
+	)
