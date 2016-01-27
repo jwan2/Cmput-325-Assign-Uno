a08dd788f488dab302ba2a3d989eefaf02c15b53
ssowemim <ssowemim@ualberta.ca>
2016-01-26 19:02:08 -0700

    more test cases

--- test.lisp
+++ test.lisp
@@ -8,6 +8,15 @@
   (xmember '(1) '((1) 2 3))
   (xmember nil nil)
 
+  (flatten '(a (b c) d))
+  (flatten '((((a)))))
+  (flatten '(a (b c) (d ((e)) f)))
+
+  (mix '(a b c) '(d e f))
+  (mix '(1 2 3) '(a))
+  (mix '((a) (b c)) '(d e f g h)) 
+  (mix '(1 2 3) nil)
+  (mix '(1 2 3) '(nil))
 
   )
 
