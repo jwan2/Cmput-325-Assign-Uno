12c6324fb6664d22957ec83882fd92dee2354632
ssowemim <ssowemim@ualberta.ca>
2016-01-26 18:58:29 -0700

    creating test case

--- test.lisp
+++ test.lisp
@@ -1,5 +1,14 @@
 
-(xmember '1 '(1))
-(xmember '1 '( (1) 2 3))
-(xmember '(1) '((1) 2 3))
-(xmember nil nil)
+(defun test ()
+
+  (load "1293430.lisp")
+
+  (xmember '1 '(1))
+  (xmember '1 '( (1) 2 3))
+  (xmember '(1) '((1) 2 3))
+  (xmember nil nil)
+
+
+  )
+
+
